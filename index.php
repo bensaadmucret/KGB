5d6e691cd261df9a59aa62eaf9350cfe0ced3c1e
mohammedBensaad <bensaadmucret@gmail.com>
2021-09-30 22:24:01 +0200

    update conformité psr

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace mzb;
 
@@ -8,9 +7,6 @@ use mzb\Router\Router;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $router = new Router();
-
-
-
 $router->setNamespace('mzb\Controller');
 $router->set404(
     function () {
@@ -30,91 +26,10 @@ $router->set404('/api(/.*)?', function () {
     echo json_encode($jsonArray);
 });
 
-
-
 $router->get('/dashboard', 'Connexion@index');
 $router->post('/admin', 'Connexion@index');
-
-
-
-
 $router->get('', 'Wellcome@index');
 $router->post('', 'Wellcome@postFormSucceeded');
-
-
-
-
-
-
-
-
-
-
-$router->run();
-
-$router->get('', 'Wellcome@index');
-$router->post('', 'Wellcome@postFormSucceeded');
-
-$router->before('GET|POST', '/admin/.*', function () {
-    if (!isset($_SESSION['admin'])) {
-        header('location: /auth/login');
-        exit();
-    }
-});
-
-
-
-
-
-
-$router->run();
-
-$router->set404(function () {
-    header('HTTP/1.1 404 Not Found');
-    echo '404 Not Found';
-});
-
-$router->set404('/api(/.*)?', function () {
-    header('HTTP/1.1 404 Not Found');
-    header('Content-Type: application/json');
-
-    $jsonArray = array();
-    $jsonArray['status'] = "404";
-    $jsonArray['status_text'] = "route not defined";
-
-    echo json_encode($jsonArray);
-});
-
-
-$router->get('/admin', 'Connexion@index');
-$router->get('', 'Wellcome@index');
-$router->post('', 'Wellcome@postFormSucceeded');
-
-$router->before('GET|POST', '/admin/.*', function () {
-    if (!isset($_SESSION['admin'])) {
-        header('location: /auth/login');
-        exit();
-    }
-});
-
-
-
-
-
-
-$router->run();
-
-$router->get('', 'Wellcome@index');
-$router->post('', 'Wellcome@postFormSucceeded');
-
-$router->before('GET|POST', '/admin/.*', function () {
-    if (!isset($_SESSION['admin'])) {
-        header('location: /auth/login');
-        exit();
-    }
-});
-
-
 
 
 
--- src/Controller/Connexion.php
+++ src/Controller/Connexion.php
@@ -19,11 +19,18 @@ class Connexion extends Controller
         $data = ['welcome, JT'] ;
        
         $form = new Form();
-        $form->start_form('/admin', 'post', '', [ 'class'=>'form-control form-text wow fadeInUp animated' ])
-        ->addFor('email', 'email')->addEmail('email', '', [ 'class'=>'form-control form-text wow fadeInUp animated', 'maxlength'=>'40'])
-        ->addFor('password', 'password')->addPassword('password', '', [ 'class'=>'form-control form-text wow fadeInUp animated', 'required'=>'true', 'minlength'=>'4'])
+        $form->start_form('/admin', 'post', '', 
+        [ 'class'=>'form-control form-text wow fadeInUp animated' ])
+        ->addFor('email', 'email')->addEmail('email', '', 
+        [ 'class'=>'form-control form-text wow fadeInUp animated', 
+        'maxlength'=>'40'])
+        ->addFor('password', 'password')
+        ->addPassword('password', '', 
+        [ 'class'=>'form-control form-text wow fadeInUp animated', 
+        'required'=>'true', 'minlength'=>'4'])
         ->addToken($key)
-        ->addBouton('Envoyer', ['class'=>'btn-primary btn mb-3 mt-3 form-button wow fadeInUp animated'])
+        ->addBouton('Envoyer', 
+        ['class'=>'btn-primary btn mb-3 mt-3 form-button wow fadeInUp animated'])
         ->end_form();
 
         $this->render('/admin/dashboard', compact('data', 'form'));
