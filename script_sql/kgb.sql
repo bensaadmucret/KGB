9876046004d2723fa30dd52cceadf9ea40fc7128
Ben <ben@aixmarketing4-BIS.local>
2021-10-10 17:50:09 +0200

    création script sql

--- script_sql/kgb.sql
+++ script_sql/kgb.sql
@@ -3,75 +3,149 @@
     USE KGB;
 
 
-    CREATE TABLE agents (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        nom TEXT NOT NULL,
-        prenom TEXT NOT NULL,
-        date_naissance TEXT NOT NULL,
-        code_identification TEXT NOT NULL,
-        nationalite TEXT NOT NULL,
-        specialite TEXT NOT NULL
-    );
 
-
-
-    CREATE TABLE cibles (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        nom TEXT NOT NULL,
-        prenom TEXT NOT NULL,
-        date_naissance TEXT NOT NULL,
-        code_identification TEXT NOT NULL,
-        nationalite TEXT NOT NULL
-    );
+    CREATE TABLE IF NOT EXISTS agent (
+        id_agent INT NOT NULL AUTO_INCREMENT,
+        nom VARCHAR(255) NOT NULL,
+        prenom VARCHAR(255) NOT NULL,
+        date_naissance DATE NOT NULL,
+        code_identification VARCHAR(255) NOT NULL,
+        nationalite VARCHAR(255) NOT NULL,
+        specialite VARCHAR(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_agent)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
 
-    CREATE TABLE contacts (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        nom TEXT NOT NULL,
-        prenom TEXT NOT NULL,
-        date_naissance TEXT NOT NULL,
-        code_identification TEXT NOT NULL,
-        nationalite TEXT NOT NULL
-    );
-
-    CREATE TABLE planques (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        code TEXT NOT NULL,
-        adresse TEXT NOT NULL,
-        pays TEXT NOT NULL,
-        type TEXT NOT NULL
-    );
+    CREATE TABLE IF NOT EXISTS cible (
+        id_cible INT NOT NULL AUTO_INCREMENT,
+        nom VARCHAR(255) NOT NULL,
+        prenom VARCHAR(255) NOT NULL,
+        date_naissance DATE NOT NULL,
+        code_identification VARCHAR(255) NOT NULL,
+        nationalite VARCHAR(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_cible)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
-    CREATE TABLE missions (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        titre TEXT NOT NULL,
-        description TEXT NOT NULL,
-        code_identification TEXT NOT NULL,
-        pays TEXT NOT NULL,
-        agent_id INTEGER NOT NULL,
-        contact_id INTEGER NOT NULL,
-        cible_id INTEGER NOT NULL,
-        type TEXT NOT NULL,
-        statut TEXT NOT NULL,
-        planque_id INTEGER NOT NULL,
-        specialite TEXT NOT NULL,
-        date_debut TEXT NOT NULL,
-        date_fin TEXT NOT NULL
-    );
+    CREATE TABLE IF NOT EXISTS contact (
+        id_contact INT NOT NULL AUTO_INCREMENT,
+        nom VARCHAR(255) NOT NULL,
+        prenom VARCHAR(255) NOT NULL,
+        date_naissance DATE NOT NULL,
+        code_identification VARCHAR(255) NOT NULL,
+        nationalite VARCHAR(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_contact)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
+    CREATE TABLE IF NOT EXISTS planque (
+        id_planque INT NOT NULL AUTO_INCREMENT,
+        code VARCHAR(255) NOT NULL,
+        adresse VARCHAR(255) NOT NULL,
+        pays VARCHAR(255) NOT NULL,
+        type_de_mission VARCHAR(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_planque)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-    CREATE TABLE administrateurs (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        nom TEXT NOT NULL,
-        prenom TEXT NOT NULL,
-        mail TEXT NOT NULL,
-        mdp TEXT NOT NULL,
-        date_creation TEXT NOT NULL
-    );
+
+
+
+
+    CREATE TABLE IF NOT EXISTS mission (
+        id_mission INT NOT NULL AUTO_INCREMENT,
+        titre VARCHAR(255) NOT NULL,
+        description_mission VARCHAR(255) NOT NULL,
+        code_identification VARCHAR(255) NOT NULL,
+        pays VARCHAR(255) NOT NULL,
+        id_agent INT NOT NULL,
+        id_contact INT NOT NULL,
+        id_cible INT NOT NULL,
+        type_de_mission VARCHAR(255) NOT NULL,
+        statut VARCHAR(255) NOT NULL,
+        id_planque INT NOT NULL,
+        specialite_requise VARCHAR(255) NOT NULL,
+        date_debut DATE NOT NULL,
+        date_fin DATE NOT NULL,
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_mission)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Sur une mission, les contacts sont obligatoirement de la nationalité du pays de la mission.
+# Sur une mission, la planque est obligatoirement dans le même pays que la mission.
+# Sur une mission, il faut assigner au moins 1 agent disposant de la spécialité requise. 
+
+
+    CREATE TABLE IF NOT EXISTS mission_agent (
+        id_mission INT NOT NULL,
+        id_agent INT NOT NULL,
+        PRIMARY KEY (id_mission, id_agent)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+    CREATE TABLE IF NOT EXISTS mission_contact (
+        id_mission INT NOT NULL,
+        id_contact INT NOT NULL,
+        PRIMARY KEY (id_mission, id_contact)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+    CREATE TABLE IF NOT EXISTS mission_cible (
+        id_mission INT NOT NULL,
+        id_cible INT NOT NULL,
+        PRIMARY KEY (id_mission, id_cible)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+    CREATE TABLE IF NOT EXISTS mission_planque (
+        id_mission INT NOT NULL,
+        id_planque INT NOT NULL,
+        PRIMARY KEY (id_mission, id_planque)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+    CREATE TABLE IF NOT EXISTS mission_specialite (
+        id_mission INT NOT NULL,
+        id_specialite INT NOT NULL,
+        PRIMARY KEY (id_mission, id_specialite)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+    
+
+# Les administrateurs ont un nom, un prénom, une adresse mail, un mot de passe, une date de création.
+
+    CREATE TABLE IF NOT EXISTS administrateur (
+        id_administrateur INT NOT NULL AUTO_INCREMENT,
+        nom VARCHAR(255) NOT NULL,
+        prenom VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password_admin VARCHAR(255) NOT NULL,
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_administrateur)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+
 
