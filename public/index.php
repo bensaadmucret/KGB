ad327241c912efd7a8ba19644c96e1d9a9ed3b98
Mohammed Bensaad <Mohammed.Bensaad@itga.fr>
2022-04-12 12:26:23 +0200

    update index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require APP_PATH . DS . 'vendor' . DS . 'autoload.php';
+require_once(APP_PATH. 'vendor'. DS .'autoload.php');
 use App\factory\AppFactory;
 //use Core\Database\Connection;
 
