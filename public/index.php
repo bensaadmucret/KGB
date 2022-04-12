6fe090dddc9df2aff69fba142a3e610f7a9cae3f
Mohammed Bensaad <Mohammed.Bensaad@itga.fr>
2022-04-12 14:49:35 +0200

    update index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,15 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+if (!defined('ROOT')) {
+    define("ROOT", dirname($_SERVER['DOCUMENT_ROOT']));
+}
+if (!defined('APP_PATH')) {
+    define("APP_PATH", ROOT . DS);
+}
 
 include_once '../vendor/autoload.php';
 use App\factory\AppFactory;
